9459c30185beefae55a30c59bbcae4221e1fb426
sylinx_zj <sylinx_zj@163.com>
2021-07-30 16:51:05 +0900

    Initial commit

---
+++ src/main/java/com/sylinx/SylinxApplication.java
@@ -0,0 +1,11 @@
+package com.sylinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylinxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SylinxApplication.class,args);
+    }
+}
